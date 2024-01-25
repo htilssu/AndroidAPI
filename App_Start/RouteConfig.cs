702e68fa77a1b1dc12e42fb11efbcf214c58c4ab
htilssu <tolashuu@gmail.com>
2024-01-25 17:58:10 +0700

    config router for api

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,11 +13,14 @@ namespace ShoppingWEBTraining
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
             routes.MapRoute(
-                name: "Default",
-                url: "/api/{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "api",
+                url: "api/{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.LowercaseUrls = true;
         }
     }
 }
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace ShoppingWEBTraining.Controllers;
+
+public class ProductController : Controller
+{
+    [HttpGet]
+    [ActionName("Index")]
+    public ActionResult? Get(int? id)
+    {
+        if (id == null)
+        {
+            //TODO return all products
+        }
+
+        Response.ContentType = "application/json";
+        Response.SetCookie(new HttpCookie("cookie", Guid.NewGuid().ToString()));
+        Response.StatusCode = (int)HttpStatusCode.OK;
+        //TODO collect product by id
+        return Json(new { id = 123, name = "product1" }, JsonRequestBehavior.AllowGet);
+    }
+
+    [HttpPost]
+    public ActionResult Post()
+    {
+        return new HttpStatusCodeResult(HttpStatusCode.OK);
+    }
+}
